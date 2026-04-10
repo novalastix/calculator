085b8da3a03ceb7021777263dc9b3ec4ed3dd556
Valerie Nova <10227076+novalastix@users.noreply.github.com>
2026-04-09 20:16:43 -0700

    basis for adding text to the screen

--- main.js
+++ main.js
@@ -1,8 +1,28 @@
 const textbar = document.getElementById("textbar");
+const screendiv = document.getElementById("screen");
 
 function insert(x)
 {
     textbar.value+=x;
     textbar.focus();
     return false;
+}
+
+function submit()
+{
+    if(textbar.value.trim().length > 0)
+    {
+        addLine(textbar.value);
+    }
+    textbar.value="";
+    textbar.focus();
+    return false;
+}
+
+function addLine(text)
+{
+    let newline = document.createElement("div");
+    newline.classList.add("line");
+    newline.textContent = text;
+    screendiv.appendChild(newline);
 }
